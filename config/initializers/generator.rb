ea59a0b2630cf688716e78e92d310fe2b7858f69
Sergey Belotelkin <serwhite@yandex.ru>
2013-01-21 10:21:35 +0400

    init project

---
+++ config/initializers/generator.rb
@@ -0,0 +1,5 @@
+Rails.application.config.generators do |g|
+  g.stylesheet_engine = :scss
+  g.javascript_engine = :coffee
+  g.template_engine = :haml
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whitecms::Application.config.secret_token = 'ce662a04aec3e5a3a423d2427bb9d81bb94db4efdbd47a3714cda4fc2955fe1dd41589283ff6cd2caeabcf35c4cf4af2de1c47b5fccd279934ee23f4d83345eb'
